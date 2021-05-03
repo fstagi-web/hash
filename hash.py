419f5a6f35e4f62ca9aca6a9a260836fac9c9606
Frank Stagi <53384374+fstagi-web@users.noreply.github.com>
2021-05-02 18:19:08 -0700

    changes

--- hash.py
+++ hash.py
@@ -1,6 +1,18 @@
 import hashlib
 
 x = input("Please enter your bank account number\n")
-hash_name = hashlib.sha512(x.encode())
+hash_name = hashlib.sha256(x.encode())
 hex_dig = hash_name.hexdigest()
+
+z = input("Please enter your SSN with no spaces or dashes \n")
+hash_name2 = hashlib.sha256(z.encode())
+hex_dig2 = hash_name2.hexdigest()
+
 print(hex_dig)
+print(hex_dig2)
+
+
+
+
+
+
