3c00f65022ff723555cb475b664d38b55834661a
Frank Stagi <53384374+fstagi-web@users.noreply.github.com>
2021-05-04 17:28:33 -0700

    Notes and 8 digit transaction id

--- hash.py
+++ hash.py
@@ -2,28 +2,39 @@ import hashlib
 import time
 import uuid
 
-print("***Bank Verification***")
+print("--------------")
+print("**Bank Verification**")
+print("--------------")
 time.sleep(1)
 
 
+# User Name
 
 name = input("Please enter your name \n")
 hash_name4 = hashlib.sha256(name.encode())
 hex_dig4 = hash_name4.hexdigest()
+
+#User Pin
 
 pin = input("Please enter your PIN\n")
 hash_name5 = hashlib.sha256(pin.encode())
 hex_digi5 = hash_name5.hexdigest()
 
 
+#User Acct Number
 
 x = input("Please enter your bank account number\n")
 hash_name = hashlib.sha256(x.encode())
 hex_dig = hash_name.hexdigest()
 
+#User SSN
+
 z = input("Please enter your SSN with no spaces or dashes \n")
 hash_name2 = hashlib.sha256(z.encode())
 hex_dig2 = hash_name2.hexdigest()
+
+
+#User Withdrawl amount
 
 y = input("Please enter the amount to withdrawl\n")
 hash_name3 = hashlib.sha256(y.encode())
@@ -31,8 +42,9 @@ hex_dig3 = hash_name3.hexdigest()
 
 time.sleep(2)
 
+
 print("---------------")
-print("*TRANSACTION ID*", uuid.uuid1())
+print("*TRANSACTION ID*", uuid.uuid4().hex[:8])
 print("---------------")
 
 time.sleep(1)
